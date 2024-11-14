da53d73c4178af01f475ea812627810fcc2b324a
maateussantana <mateussantana3626@gmail.com>
2024-11-14 20:55:05 -0300

    Criando tela inicial.

--- app/src/main/java/com/example/gowork/views/SignUpActivity.kt
+++ app/src/main/java/com/example/gowork/views/SignUpActivity.kt
@@ -1,11 +1,9 @@
-package com.example.gowork
+package com.example.gowork.views
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gowork.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -13,19 +11,22 @@ class SignUpActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
+    private lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
         firestore = FirebaseFirestore.getInstance()
 
-        val nameEditText = findViewById<EditText>(R.id.etName)
-        val emailEditText = findViewById<EditText>(R.id.etEmail)
-        val passwordEditText = findViewById<EditText>(R.id.etPassword)
-        val signInButton = findViewById<Button>(R.id.btnSignIn)
-        val loginRedirectText = findViewById<TextView>(R.id.tvLoginRedirect)
+        val nameEditText = binding.etName
+        val emailEditText = binding.etEmail
+        val passwordEditText = binding.etPassword
+        val signInButton = binding.btnSignIn
+        val loginRedirectText = binding.tvLoginRedirect
 
         signInButton.setOnClickListener {
             val name = nameEditText.text.toString().trim()
@@ -63,7 +64,7 @@ class SignUpActivity : AppCompatActivity() {
         }
 
         loginRedirectText.setOnClickListener {
-
+            // Implementar ação para redirecionar para login (caso necessário)
         }
     }
 }
