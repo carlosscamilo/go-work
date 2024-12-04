acd9b040dac0f7d3f017ce720a404a5ebdbe82be
CarlosHoland4 <carlosfernandohcosta5@gmail.com>
2024-12-04 17:49:01 -0300

    criando tela de perfil

---
+++ app/src/main/java/com/example/gowork/PerfilActivity.kt
@@ -0,0 +1,16 @@
+package com.example.gowork
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class PerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_perfil)
+    }
+}
